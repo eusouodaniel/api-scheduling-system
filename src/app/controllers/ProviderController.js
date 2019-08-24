63017242eea480998eb4eea0e55d5213de756142
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2019-08-24 13:56:03 -0300

    new(): List providers services and table files associated with users

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,22 @@
+import File from '../models/File';
+import User from '../models/User';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'file_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'name', 'path', 'url'],
+        },
+      ],
+    });
+
+    res.json(providers);
+  }
+}
+
+export default new ProviderController();
