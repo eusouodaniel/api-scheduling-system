f7d5dc38b7431a69eedae6802b125b78e4b9eec9
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2019-08-25 12:38:29 -0300

    new(): Configure cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
@@ -20,6 +21,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
